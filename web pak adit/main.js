e98170aed307550f423e7a43ff6cf5a1b9dc5fb8
Bezethade <149304208+Bezethade@users.noreply.github.com>
2024-03-13 05:40:12 +0700

    baruuuuuuu

--- web pak adit/main.js
+++ web pak adit/main.js
@@ -1,17 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const navbar = document.getElementById("navbar");
 
-  // Set a scroll event listener
-  window.addEventListener("scroll", function () {
-    // Add a background class when scrolling
-    if (window.scrollY > 0) {
-      navbar.classList.add("navbar-scrolled");
-    } else {
-      // Remove the background class when at the top
-      navbar.classList.remove("navbar-scrolled");
-    }
-  });
-});
+
 
 document.addEventListener("DOMContentLoaded", function () {
   // Inisialisasi Lightbox
@@ -29,6 +17,25 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const filterContainer = document.querySelector(".gallery-filter");
 const galleryItem = document.querySelectorAll(".gallery-item");
+
+// Fungsi untuk menginisialisasi filter berdasarkan kelas yang aktif
+function initializeFilter() {
+  const activeFilter = filterContainer.querySelector(".active");
+  const filterValue = activeFilter.getAttribute("data-filter");
+
+  galleryItem.forEach((item) => {
+    if (item.classList.contains(filterValue) || filterValue === "all") {
+      item.classList.remove("hide");
+      item.classList.add("show");
+    } else {
+      item.classList.remove("show");
+      item.classList.add("hide");
+    }
+  });
+}
+
+// Panggil fungsi inisialisasi saat halaman pertama dimuat
+document.addEventListener("DOMContentLoaded", initializeFilter);
 
 filterContainer.addEventListener("click", (event) => {
   if (event.target.classList.contains("filter-item")) {
@@ -48,4 +55,6 @@ filterContainer.addEventListener("click", (event) => {
     });
   }
 });
+
+
 
